1243620fbfb74271d9db8a1f6aebf9ea56dc5e23
Blake <blakecaven@outlook.com>
2023-11-26 12:37:59 -0500

    script to remove the default route - this is... of course... not actually useful because there are 'proper' ways to do networking

--- remove_default_route.sh
+++ remove_default_route.sh
@@ -2,11 +2,5 @@
 
 # in linux, ethernet should take priority, so this script is here to remove that from the routing table (lol)
 
-default_device=`route -n | awk 'NR==3{print $8;exit}'`
-wifi_device="wlp3s0"
-if [[ "$default_device" != "$wifi_device" ]]; then
-	echo "not using wifi"
-	route del default
-else
-	echo "using wifi as default"
-fi
+default_device=`route -n | awk "NR==3{print;exit}" | grep -E en`
+echo $default_device
