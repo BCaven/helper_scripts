65c00150a679d28d8ff9e7420327fe797393133b
BCaven <blakecaven@outlook.com>
2023-11-26 04:04:17 -0500

    built watchdog utility to be strong when I am weak

---
+++ watchdog.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+
+bed_time="2300"
+wake_time="0600"
+watchlist="watchlist.txt"
+woken_up="false"
+CRON=""
+
+while [ $# -gt 0 ]
+do
+    case $1 in
+        -b) shift; bed_time="$1";;
+        -w) shift; wake_time="$1";;
+        -l) shift; watchlist="$1";;
+        *) echo "`basename $0`: usage [-b HHMM | -w HHMM | -l FILENAME]"; exit 1;;
+    esac
+done
+
+while [ "$woken_up" = "false" ] ;
+do
+    time=`date +%H%M`
+    if [ "$time" -gt "$bed_time" ] ; then
+        echo "normal mode - past bedtime"
+        for item in `cat $watchlist`;
+        do
+            killall $item
+        done
+    elif [ "$time" -lt "$wake_time" ] ; then
+        echo "normal model - before wake time"
+        for item in `cat $watchlist`;
+        do
+            killall $item
+        done
+    else
+        woken_up="true"
+    fi
+    sleep 5m
+done
