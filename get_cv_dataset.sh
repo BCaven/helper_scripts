ca87d751a2cc0d8690dacb270814b042dfb5cc87
Blake <jcaven@nd.edu>
2024-11-10 13:39:57 -0500

    fixed off by one error

--- get_cv_dataset.sh
+++ get_cv_dataset.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 wget https://www.idiap.ch/webarchives/sites/www.idiap.ch/resource/gestures/data/BochumGestures1998.tar.gz
-tar -xvzf BochumGestures1998.tar.gz
+tar -xzf BochumGestures1998.tar.gz
 rm BochumGestures1998.tar.gz
 cd BochumGestures1998/
 
 
 # grab all of the things for each class
 
-classes=`ls sih/ | sed -E 's/[a-z23]*([0-9]{2})([a-z])00R.tiff/\1/g' | sort -u`
+classes=`ls sih/ | grep -v "ROTATION" | sed -E 's/[a-z23]*([0-9]{2})([a-z])00R.tiff/\1/g' | sort -u`
 mkdir train_sih/
 for class in $classes; do
-    class=$(($class-1))
+    class_label=$class
+    class=`expr $class - 1`
     counter=0
     mkdir train_sih/$class/
     # grab every file that matches that class
     # and add it to the new directory
-    for file in `ls sih/ | grep $class | sed -E 's/.tiff//g'`; do
+    for file in `ls sih/ | grep $class_label | sed -E 's/.tiff//g'`; do
         cp sih/$file.tiff train_sih/$class/$counter.tiff
         counter=$(($counter+1))
     done
@@ -27,15 +28,16 @@ echo "sih images done"
 
 # grab all of the things for each class
 
-classes=`ls rgb/ | sed -E 's/[a-z23]*([0-9]{2})([a-z])00R.tiff/\1/g' | sort -u`
+classes=`ls rgb/ | grep -v "ROTATION" | sed -E 's/[a-z23]*([0-9]{2})([a-z])00R.tiff/\1/g' | sort -u`
 mkdir train_rgb/
 for class in $classes; do
-    class=$(($class-1))
+    class_label=$class
+    class=`expr $class - 1`
     counter=0
     mkdir train_rgb/$class/
     # grab every file that matches that class
     # and add it to the new directory
-    for file in `ls rgb/ | grep $class | sed -E 's/.tiff//g'`; do
+    for file in `ls rgb/ | grep $class_label | sed -E 's/.tiff//g'`; do
         cp rgb/$file.tiff train_rgb/$class/$counter.tiff
         counter=$(($counter+1))
     done
