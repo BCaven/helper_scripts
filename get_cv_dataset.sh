c4cb01b2bba61129f3e4523794d2c86889edcd26
Blake C <blakecaven@outlook.com>
2024-11-10 12:49:41 -0500

    Create get_cv_dataset.sh
    adding helper script to download and format the dataset

---
+++ get_cv_dataset.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+wget https://www.idiap.ch/webarchives/sites/www.idiap.ch/resource/gestures/data/BochumGestures1998.tar.gz
+tar -xvzf BochumGestures1998.tar.gz
+rm BochumGestures1998.tar.gz
+cd BochumGestures1998/
+
+echo "only using sih images for now"
+
+# grab all of the things for each class
+
+classes=`ls sih/ | sed -E 's/[a-z23]*([0-9]{2})([a-z])00R.tiff/\1/g' | sort -u`
+mkdir train_sih/
+for class in $classes; do
+    counter=0
+    mkdir train_sih/$class/
+    # grab every file that matches that class
+    # and add it to the new directory
+    for file in `ls sih/ | grep $class | sed -E 's/.tiff//g'`; do
+        cp sih/$file.tiff train_sih/$class/$counter.tiff
+        counter=$(($counter+1))
+    done
+done
+rm -rf sih/
+# and do it again for rgb
+
+# grab all of the things for each class
+
+classes=`ls rgb/ | sed -E 's/[a-z23]*([0-9]{2})([a-z])00R.tiff/\1/g' | sort -u`
+mkdir train_rgb/
+for class in $classes; do
+    counter=0
+    mkdir train_rgb/$class/
+    # grab every file that matches that class
+    # and add it to the new directory
+    for file in `ls rgb/ | grep $class | sed -E 's/.tiff//g'`; do
+        cp rgb/$file.tiff train_rgb/$class/$counter.tiff
+        counter=$(($counter+1))
+    done
+done
+rm -rf rgb/
